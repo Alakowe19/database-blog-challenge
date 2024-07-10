ebc3ca0c8c4af0396e26630f5ae1cca8c7ad5008
Oluwagbemi Abiodun <oluwagbemiiabiodun@gmail.com>
2024-07-10 18:47:36 +0100

    core added

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,21 +2,69 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function seed() {
-    const createdUsers = await prisma.user.createMany({
-        data: [
-            { username: 'Bob' }, 
-            { username: 'alice' } 
+    const createdUsers = await prisma.user.create({
+        data: {
+            username: 'Bob',
+            email: 'bob@example.com',
+            profile: {
+                create: {
+                    bio: 'I am learning'
+                }
+            }
+        }, 
+        
+        include: {
+            profile: true
+        }
+    })
+
+    console.log(`${createdUsers.username} users created`, createdUsers)
+
+    const userPosts = await prisma.post.createMany({
+        data: [ 
+            {
+              title: 'My Title', 
+              content: 'I said this ...',  
+              userId: createdUsers.id
+            }, 
+            {
+                title: 'Boolean', 
+                content: 'The great halo ...', 
+                userId: createdUsers.id
+              }, 
+              {
+                title: 'Post me', 
+                content: 'Whats the great ...',
+                userId: createdUsers.id
+              }
         ]
-    });
+    })
+            
+    console.log(`${userPosts.count} post created`, userPosts  )
 
-    console.log(`${createdUsers.count} users created`, createdUsers);
+    const userComments = await prisma.comment.createMany({
+        data: [
+            {
+                content:  'creating comments ...',
+                userId: createdUsers.id
+            },
 
-    // Add your code here
+            {
+                content: 'Boolean is the best ...',
+                userId: createdUsers.id
+            }, 
 
-    
+            {
+                content: 'Way loading .. ...',
+                userId: createdUsers.id
+            }
+          
+        ]
+    })
 
+    console.log(`${userComments.count} comment created`, userComments)
 
-    // Don't edit any of the code below this line
+    // Don't edit any of the  code below this line
     process.exit(0);
 }
 
